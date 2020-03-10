2d5d57afaa07ee66bb49d88a67ea997dffccc1a4
Juan Carlos <jc27341@hotmail.com>
2020-03-10 12:58:33 -0500

    feat(notifier):Add function to report of total saved

--- handler.js
+++ handler.js
@@ -5,13 +5,21 @@ var current_week = require('current-week-number');
 
 aws.config.update({region:'us-east-1'});
 
+function getTotalSaved(){
+    let total = 0
+    for(let i=1; i<=current_week(); i++){
+        total = total + i
+    }
+    return total
+}
+
 function sendNotification(){
   
+  let totalSaved = getTotalSaved()
   var params = {
-    Message: 'Esta semana toca poner '+current_week()+' soles en la alcancia :)',
+    Message: 'Esta semana toca poner '+current_week()+' soles en la alcancia y tendras ahorrado '+totalSaved+' soles :)',
     TopicArn: 'arn:aws:sns:us-east-1:530088275162:moneybox'
   }
-
   var publishText = new aws.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
   publishText.then(
